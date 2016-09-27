29aeed3b04823fe3e3b95bcb7f45c3812b1fc8d3
Will Hogan <willhogan11@hotmail.com>
2016-09-27 10:59:59 +0100

    Added Spring, dispatcher(W.I.P) dependencies

---
+++ src/ie/gmit/sw/controller/UserController.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("/")
+	public String showUsers(){
+		return "users";
+	}
+}
