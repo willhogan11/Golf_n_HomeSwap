d7f01c64986c05e251dcd1be237e9d446ff361e4
Will Hogan <willhogan11@hotmail.com>
2016-10-08 00:06:13 +0100

    Extra commit of unstaged changes

--- src/ie/gmit/sw/testing/RobustMongodbTest.java
+++ src/ie/gmit/sw/testing/RobustMongodbTest.java
@@ -1,4 +1,4 @@
-package ie.gmit.sw.config;
+package ie.gmit.sw.testing;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
@@ -10,12 +10,9 @@ public class RobustMongodbTest {
 	
 	public static void main(String[] args) {
 		try {
-			
 			// Connect to mongodb server on localhost
 			MongoClient mongoClient = new MongoClient(HOST, PORT);
-			
 			DB db = mongoClient.getDB("testGolfNSwap");
-			
 			System.out.println("Successfully connected to MongoDB database " + db);
 			
 		} catch (Exception e) {
--- src/ie/gmit/sw/testing/SpringMongoConfig.java
+++ src/ie/gmit/sw/testing/SpringMongoConfig.java
@@ -1,15 +1,16 @@
-package ie.gmit.sw.config;
+package ie.gmit.sw.testing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
 import com.mongodb.MongoClient;
 
 @Configuration
 public class SpringMongoConfig {
-	
+
 	public @Bean
 	MongoDbFactory mongoDbFactory() throws Exception {
 		return new SimpleMongoDbFactory(new MongoClient(), "golfnswap");
