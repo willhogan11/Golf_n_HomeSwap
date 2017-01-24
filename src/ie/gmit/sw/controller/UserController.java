373a8baf85a9c0893fdc593b3963d717d5ac426b
Andrej Lavrinovic <andrei.lavrinovic@gmail.com>
2017-01-24 15:08:14 +0000

    issue #32 is done.

--- src/ie/gmit/sw/controller/UserController.java
+++ src/ie/gmit/sw/controller/UserController.java
@@ -52,44 +52,57 @@ public class UserController {
 		return "admin"; 
 	}
 	
+	/**
+	 * 
+	 * Controller approveUser activated after pressing Approve button on admin page.
+	 * If administrator approved the application user's status changed on REGISTERED, 
+	 * button itself replaced by label with note "APPROVE" and user getting email with
+	 * confirmation letter. 
+	 * 
+	 * @param request contains data from uri string that is after '?' mark.
+	 * @param model container for data that need to be transfered to jsp page
+	 * @return Mapping associated page (admin.jsp) and navigate user onto this page.
+	 * 
+	 */
 	// Approve user's application for membership
 	@RequestMapping(value="/approve", method=RequestMethod.GET)
 	public String approveUser(HttpServletRequest request, Model model){
+		// get all users
+		List<User> users = userRepo.findAllUsers();
 		
 		// get user that been chosen for approvement
 		User u = userRepo.findUserById(request.getParameter("u"));
-		
-		//**************************************************************************
-		// Change access level for user here
-		//**************************************************************************
-		// ... //
-		u.setUseraccesslevel(UserAccessLevel.REGISTERED);
-		userRepo.updateUser(u);
-		log.info("User's access level is " + u.getUseraccesslevel());
-		
-		//**************************************************************************
-		// This section is for sending email to user for confirmation of approvement.
-		//**************************************************************************
+
+		// set all components for sending email to the user.
 		String message = "Memebership for user " + u.getFirstname() + " " + u.getSurname() + "is accepted and approved.";
 		String to = u.getEmail();
 		String subject = "Membership";
 		
-		Emailable email = new EmailSender(to, subject, message);
-		
-		// Sending email
-		try{
-			mailSender.send(email.getSmm());
-			log.info("Mail sent to " + u.getEmail());
-		}catch(Exception e){
-			log.info("Error with sending");
-			log.info(e.getMessage().toString());
+		// Do approve user
+		if(!UserAccessLevel.REGISTERED.equals(u.getUseraccesslevel())){
+			//**************************************************************************
+			// Change access level for user here
+			//**************************************************************************
+			u.setUseraccesslevel(UserAccessLevel.REGISTERED);
+			userRepo.updateUser(u);
+			
+			//**************************************************************************
+			// This section is for sending email to user for confirmation of approvement.
+			//**************************************************************************
+			Emailable email = new EmailSender(to, subject, message);
+			
+			// Sending email
+			try{
+				mailSender.send(email.getSmm());
+				log.info("Mail sent to " + u.getEmail());
+			}catch(Exception e){
+				log.info("Error with sending");
+				log.info(e.getMessage().toString());
+			}
 		}
+		log.info("User's access level is " + u.getUseraccesslevel());
 		
-		// Populate admin page with users
-		// get all users
-		List<User> users = userRepo.findAllUsers();
-		
-		// users binding
+		// populate user's table.
 		model.addAttribute("users", users);
 		
 		return "admin";
